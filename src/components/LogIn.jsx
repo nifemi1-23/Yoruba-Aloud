1ae422948364b4af3ee06e7ffb5b16ab1ca0a41a
Nifemi Otubanjo <nifemiotubanjo@gmail.com>
2025-06-01 12:49:40 +0100

    Login and Sign Up Pages updated with API Integration

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {Link} from "react-router-dom";
+import Swal from 'sweetalert2';
 import bgImg from "/assets/images/spec.png";
 
 
-function LogIn() {
-        const myStyle = {
-            backgroundImage: `url(${bgImg})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            backgroundSize: "cover"
-        };
+const LogIn = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('https://cyrilyoruba.juadebgabriel.com/yorubalearning/api/admin_login', formData);
+            console.log('Login successful:', response.data);
+            if (response.status === 200) {
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Login successful. Welcome back!',
+                    icon: 'success',
+                    confirmButtonText: 'Okay',
+                    confirmButtonColor: 'green'
+                });
+
+                // clear form
+                setFormData({
+                    email: '',
+                    password: '',
+                });
+                // Redirect to dashboard or home page
+                // window.location.href = '/'; 
+            }
+        } catch (error) {
+            console.error('Login failed:', error.response?.data || error.message);
+            Swal.fire({
+                title: 'Error!',
+                text: error.response?.data?.message || 'Login failed. Please check your credentials.',
+                icon: 'error',
+                confirmButtonText: 'Okay',
+                cancelButtonColor:'red'
+            });
+        }
+
+    };
+
+    const myStyle = {
+        backgroundImage: `url(${bgImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover"
+    };
+
     return (
         <main className="Main">
             <div className="row">
@@ -23,14 +71,14 @@ function LogIn() {
                 </div>
                 <div style={myStyle} className="col-xs-12 col-sm-12 col-md-12 col-lg-7 col-xl-7 img_bg2">
                     <h2 className="text-center mt-5 head">Login</h2>
-                    <form className="signIn">
+                    <form className="signIn" onSubmit={handleLogin}>
                         <label htmlFor="Email" className="font-mulish font-size-14 font-weight-600">Email
                             Address</label>
-                        <input type="email" name="email" id="email" className="input mb-3" placeholder="Enter Email Address"/>
+                        <input type="email" name="email" id="email" onChange={handleChange} className="input mb-3" placeholder="Enter Email Address"/>
 
                         <label htmlFor="Password" className="font-mulish font-size-14 font-weight-600">Password</label>
-                        <input type="password" name="password" id="password" className="input mb-3" placeholder="Enter Password"/>
-                        <button className="Btn linkBtn"><Link className="linkText" to="/">Log In</Link></button>
+                        <input type="password" name="password" id="password" onChange={handleChange} className="input mb-3" placeholder="Enter Password"/>
+                        <button className="Btn linkBtn"><span className="linkText" >Log In</span></button>
                         <button className="Btn mt-2 linkBtn2"><Link className="linkText2" to="/SignUp">Sign Up</Link></button>
                     </form>
                 </div>
@@ -38,6 +86,6 @@ function LogIn() {
 
         </main>
     );
-}
+};
 
 export default LogIn;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {Link} from "react-router-dom";
+import Swal from 'sweetalert2';
 import bgImg from "/assets/images/spec.png"
 
+const SignUp = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-function SignUp() {
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    }
+
+    const handleSubmit = async (e) => { 
+        e.preventDefault();
+        try {
+            const response = await axios.post('https://cyrilyoruba.juadebgabriel.com/yorubalearning/api/register_admin', formData);
+            console.log('Registration successful:', response.data);
+            if (response.status === 200) {
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Registration successful. You can now log in.',
+                    icon: 'success',
+                    confirmButtonText: 'Okay'
+                });
+                // clear form and Redirect to login
+                setFormData({
+                    name: '',
+                    email: '',
+                    password: '',
+                    confirmPassword: ''
+                });
+                // Redirect to login page
+                window.location.href = '/'; 
+            
+            }
+        } catch (error) {
+            console.error('Registration failed:', error.response?.data || error.message);
+            Swal.fire({
+                title: 'Error!',
+                text: error.response?.data?.message || 'Registration failed. Please try again.',
+                icon: 'error',
+                confirmButtonText: 'Okay'
+            });
+        }
+    }
+
     const myStyle = {
         backgroundImage: `url(${bgImg})`,
         backgroundRepeat: "no-repeat",
@@ -21,32 +72,33 @@ function SignUp() {
                 </div>
                 <div style={myStyle} className="col-xs-12 col-sm-12 col-md-12 col-lg-7 col-xl-7 img_bg2">
                     <h2 className="text-center mt-5 head">Register</h2>
-                    <form className="signIn">
+                    <form className="signIn" onSubmit={handleSubmit}>
                         <label htmlFor="Name" className="font-mulish font-size-14 font-weight-600">Name</label>
-                        <input type="text" name="name" id="name" className="input mb-3" placeholder="Enter Name"/>
+                        <input type="text" name="name" id="name" onChange={handleChange} className="input mb-3" placeholder="Enter Name"/>
 
                         <label htmlFor="Email" className="font-mulish font-size-14 font-weight-600">Email
                             Address</label>
-                        <input type="email" name="email" id="email" className="input mb-3"
+                        <input type="email" name="email" id="email" onChange={handleChange} className="input mb-3"
                                placeholder="Enter Email Address"/>
 
                         <label htmlFor="Password" className="font-mulish font-size-14 font-weight-600">Password</label>
-                        <input type="password" name="password" id="password" className="input mb-3"
+                        <input type="password" name="password" id="password" onChange={handleChange} className="input mb-3"
                                placeholder="Enter Password"/>
 
                         <label htmlFor="Password" className="font-mulish font-size-14 font-weight-600">Confirm
                             Password</label>
-                        <input type="password" name="password" id="confirmPassword" className="input mb-3"
+                        <input type="password" name="password" id="confirmPassword" onChange={handleChange} className="input mb-3"
                                placeholder="Confirm Password"/>
                         <button className="Btn linkBtn">
-                            <Link class="linkText" to="#">Create Account</Link>
+                            Create Account
                         </button>
+                        <p className='text-dark mt-4 text-center'>Have an Account? <Link className='linkText' to="/">Login</Link></p>
                     </form>
                 </div>
             </div>
 
         </main>
     );
-}
+};
 
 export default SignUp;
